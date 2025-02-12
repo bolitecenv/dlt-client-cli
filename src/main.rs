793c39581a0509a0be0d174ffc87fc02bc64418b
Toshinori Tamashiro <boliteconline@gmail.com>
2025-02-13 01:42:01 +0900

    modify the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self, Write, Read};
 use std::net::TcpStream;
 
-use dlt_format_parser::{ DltParse, dlt_analyze };
+use dlt_format_parser::{ DltParse, DltFormat };
 
 fn main() {
     println!("Hello, world!");
@@ -13,18 +13,31 @@ fn main() {
     while true {
         let mut buffer = [0; 1024];
         let n = stream.read(&mut buffer).unwrap();
-        let (header, header_extra, remaining_data) = buffer.dlt_parse();
-        println!("{:?}", header);
-        println!("{:?}", header_extra);
-        println!("{:?}", header.get_htyp());
-        println!("{}", header.get_version());
+        let received_data = &buffer[..n];
+        println!("{:?}", received_data);
+        println!(" received size {}", n);
+        
+        let dlt_analyzed_data: Vec<DltFormat> = received_data.dlt_parse();
+
+        //println!("{:?}", dlt_analyzed_data);
+        for element in &dlt_analyzed_data {
+            println!("{:?}", element.standard_header);
+            println!("{:?}", element.standard_header_extra);
+            println!("{:?}", element.standard_header_extra_nosession_id);
+            println!("{:?}", String::from_utf8_lossy(&element.payload));
+        }
+
+        
+        
+        // println!("{:?}", header_extra);
+        // println!("{:?}", header.get_htyp());
+        // println!("{}", header.get_version());
         // if ( header.get_htyp() == UEH_MASK)
         // {
         //     println!("{}", true);
         // }
-        println!("ret val: {}", dlt_analyze(&header));
-        header_extra.debug_print();
-        println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
+       // println!("ret val: {}", dlt_analyze(&header));
+        //header_extra.debug_print();
+        //println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
     }
-    
 }
